a610daef5be9e09f1902cfe18ad7e57d5e044d73
Dexter Ketchum <silcore@hotmail.com>
2016-10-15 23:55:28 -0400

    Minor changes; attempting to write data

--- website/app.js
+++ website/app.js
@@ -1,27 +1,30 @@
 (function() {
-
-
-
   // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyB_rPPGzLDgkO7ohjDRSUddvjhDk120fWg",
-    authDomain: "mycentest.firebaseapp.com",
-    databaseURL: "https://mycentest.firebaseio.com",
-    storageBucket: "mycentest.appspot.com",
-    messagingSenderId: "308805634952"
+    apiKey: "AIzaSyCliDQbzo_m9ZdVSbes7qrlMx6ZIdPEFcw",
+    authDomain: "cen4020informe.firebaseapp.com",
+    databaseURL: "https://cen4020informe.firebaseio.com",
+    storageBucket: "cen4020informe.appspot.com",
+    messagingSenderId: "464639354758"
   };
+  firebase.initializeApp(config);
 
-	firebase.initializeApp(config);
-  
 	var urlSpecified = document.getElementById('urlSpef');
 	var articleTitle = document.getElementById('articleTitle');
 	
 	var urlSpef = firebase.database().ref().child('DBurlSpef');
-	var dbRef = firebase.database().ref().child('DBarticleTitle');
+	var dbTitle = firebase.database().ref().child('DBarticleTitle');
 	
 	urlSpef.on('value', snap => urlSpecified.innerText = snap.val());
-	dbRef.on('value', snap => articleTitle.innerText = snap.val());
+	dbTitle.on('value', snap => articleTitle.innerText = snap.val());
 	
 	firebase.database().ref().child('DBarticleSummary').on('value', snap => articleSummary.innerText = snap.val());
 	firebase.database().ref().child('DBcitation').on('value', snap => citation.innerText = snap.val());
 }());
+
+// How the hell do we do write data from the input box to the database?
+function storeURL(urlSpef) {
+  firebase.database().ref('cen4020informe/' + DBurlSpef).set({
+    DBurlSpef: urlSpef
+  });
+}
