c9838154a0f653ac41cbe0abba3c1c45a2f5fcb8
Toygeron <roybesse@comcast.net>
2016-10-17 12:59:58 -0400

    Implemented upload txt to firebase on click

--- website/app.js
+++ website/app.js
@@ -1,27 +1,35 @@
 (function() {
-
-
-
   // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyB_rPPGzLDgkO7ohjDRSUddvjhDk120fWg",
-    authDomain: "mycentest.firebaseapp.com",
-    databaseURL: "https://mycentest.firebaseio.com",
-    storageBucket: "mycentest.appspot.com",
-    messagingSenderId: "308805634952"
+    apiKey: "AIzaSyCliDQbzo_m9ZdVSbes7qrlMx6ZIdPEFcw",
+    authDomain: "cen4020informe.firebaseapp.com",
+    databaseURL: "https://cen4020informe.firebaseio.com",
+    storageBucket: "cen4020informe.appspot.com",
+    messagingSenderId: "464639354758"
   };
+  firebase.initializeApp(config);
 
-	firebase.initializeApp(config);
-  
 	var urlSpecified = document.getElementById('urlSpef');
 	var articleTitle = document.getElementById('articleTitle');
 	
 	var urlSpef = firebase.database().ref().child('DBurlSpef');
-	var dbRef = firebase.database().ref().child('DBarticleTitle');
+	var dbTitle = firebase.database().ref().child('DBarticleTitle');
 	
 	urlSpef.on('value', snap => urlSpecified.innerText = snap.val());
-	dbRef.on('value', snap => articleTitle.innerText = snap.val());
+	dbTitle.on('value', snap => articleTitle.innerText = snap.val());
 	
+	// Long form of live updating the database
 	firebase.database().ref().child('DBarticleSummary').on('value', snap => articleSummary.innerText = snap.val());
 	firebase.database().ref().child('DBcitation').on('value', snap => citation.innerText = snap.val());
+	
+	
 }());
+	
+
+// Written by Ernie 10/17/2016
+function storeURL() {
+	var url = document.getElementById('urlSpef').value;	// Store the value of the text field
+	var dbURL = firebase.database().ref().child('DBurlSpef');	// We want to put our data at the main db, at child DBurlSpef
+	
+	dbURL.set(url);	// Set the value of our child to the value in url, aka our text field
+}
