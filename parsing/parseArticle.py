4af3f40851f85bdac167cba4e212467ea9e665d3
Austin Casale <alc13g@my.fsu.edu>
2016-10-19 16:29:40 -0400

    refactor code into function

--- parsing/parseArticle.py
+++ parsing/parseArticle.py
@@ -3,6 +3,44 @@ from bs4 import BeautifulSoup
 import requests, time, re, sys
 # Input any article on https://www.theguardian.com/
 
+def getBody(url):
+	# Make our request to our URL
+	response = requests.get(url)
+
+	# Check response and declare soup object
+	try:
+		if response.ok:
+			html = response.content
+			soup = BeautifulSoup(html, 'html.parser')
+	except NameError:
+		print "No URL entered"
+		sys.exit()
+
+	# Sleep to avoid spamming requests and getting timed out, don't be a dick
+	time.sleep(.2)
+
+	# Prettify is used to print the html for the url in a way that is easier to read
+	#print soup.prettify()
+
+	# Need logic to find tag with most <p> inside
+
+	# Find <div class = "whatever name is"
+	text = soup.find("div", "content__article-body from-content-api js-article__body")
+
+
+	#using .text because the type(text) is a bs4 obj
+	# sub can remove elements
+	try:
+		x = re.sub('<[^<]+?>', "", text.text)
+		x = re.sub('\n', "", text.text)
+	except AttributeError:
+		print "Shit, couldn't find anything to parse"
+		sys.exit()
+
+	print x
+	# Print article title
+	print "\n\nTitle: ", soup.title.text
+
 url = raw_input("Please enter the url: ")
 
 # Two ideas:
@@ -14,43 +52,9 @@ url = raw_input("Please enter the url: ")
 # the article body, this approach would be better if possible
 # wouldn't need a template for every site being accessed (buzzfeed, guardian,foxnews, etc..)
 
-# Make our request to our URL
-response = requests.get(url)
 
 
-# Check response and declare soup object
-try:
-	if response.ok:
-		html = response.content
-		soup = BeautifulSoup(html, 'html.parser')
-except NameError:
-	print "No URL entered"
-	sys.exit()
-
-# Sleep to avoid spamming requests and getting timed out, don't be a dick
-time.sleep(.2)
-
-# Prettify is used to print the html for the url in a way that is easier to read
-#print soup.prettify()
-
-# Need logic to find tag with most <p> inside
-
-# Find <div class = "whatever name is"
-text = soup.find("div", "content__article-body from-content-api js-article__body")
-
-
-#using .text because the type(text) is a bs4 obj
-# sub can remove elements
-try:
-	x = re.sub('<[^<]+?>', "", text.text)
-	x = re.sub('\n', "", text.text)
-except AttributeError:
-	print "Shit, couldn't find anything to parse"
-	sys.exit()
-
-print x
-# Print article title
-print "\n\nTitle: ", soup.title.text
+getBody(url)
 
 
 
