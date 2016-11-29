0d3f301ef0f7501a047575e47f50c1f87ce2647a
snverse <tinverse@gmail.com>
2016-11-29 17:05:49 -0500

    Merge remote-tracking branch 'origin/Android' into Android
    # Conflicts:
    #	DummyDB/.idea/gradle.xml
    #	DummyDB/app/src/main/java/com/example/celiachu/dummydb/dummy_data.java
    #	DummyDB/build.gradle

---
+++ Informe_app/app/src/main/java/fyi/informe/informe_app/MainActivity.java
@@ -0,0 +1,132 @@
+package fyi.informe.informe_app;
+//Copy from here to the end, skip the package name because it might have trouble compiling on your computer.
+//Package name also differs in AndroidMAnifest file and the res folder.
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.widget.Button;
+import android.app.Activity;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.SocketTimeoutException;
+import java.net.URL;
+
+
+public class MainActivity extends Activity {
+
+    //Creating Views & Widgets to connect to UI elements
+    private EditText eMailorPhone, urlToSummarize;
+    private Button submiteMailorPhone, submitURL;
+    private TextView replyFromServer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Linking variables in code to UI elements
+        eMailorPhone = (EditText) findViewById(R.id.eMailorPhone);
+        urlToSummarize = (EditText) findViewById(R.id.urlToSummarize);
+
+        //submiteMailorPhone = (Button) findViewById(R.id.submitMailorPhone);
+        submitURL = (Button) findViewById(R.id.submitURL);
+
+        replyFromServer = (TextView) findViewById(R.id.replyFromServer);
+
+        //onClick listeners to handle what happens when their respective buttons are clicked
+        /*submiteMailorPhone.setOnClickListener (new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String LoginInfo = eMailorPhone.getText().toString();
+                Log.v("Test", LoginInfo);
+            }
+        });*/
+
+        submitURL.setOnClickListener (new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String summarizeThis = urlToSummarize.getText().toString();
+
+                myRunner postRun = new myRunner();
+
+                postRun.execute(summarizeThis);
+            }
+
+        });
+
+    }
+
+    //AsyncTask responsible for POST methods. Needs to be placed in AsyncTask because the Main
+    //Thread can not be held up with loading taskss; needs to happen in the background.
+    private class myRunner extends AsyncTask<String, String, String> {
+
+        //Main Task. Executes in another thread, in the background.
+        @Override
+        protected String doInBackground(String... urlToSummarize) {
+
+            HttpURLConnection client = null;
+            StringBuilder replyFromServer = new StringBuilder();
+
+            //HTTP Connections operations need to be placed in a try block in case an error is encountered
+            try {
+
+                URL url = new URL("http://posttestserver.com/post.php"); //www.informe.fyi:5000
+                client = (HttpURLConnection) url.openConnection();
+
+                //Set properties for the HTTP Request
+                client.setRequestMethod("POST");
+                client.setDoOutput(true);
+                client.setDoInput(true);
+
+                //Encode the request, UTF-8
+                byte[] encodedRequest = ("URL=" + urlToSummarize[0]).getBytes("UTF-8");
+
+                //Write to OutputSteam
+                OutputStream os = client.getOutputStream();
+                os.write(encodedRequest);
+                os.flush();
+                os.close();
+                int responseCode = client.getResponseCode();
+
+                //Read from InputStream, Build the string line by line
+                BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                String line;
+                while((line = br.readLine()) != null){
+                    replyFromServer.append(line);
+                }
+
+            } catch (MalformedURLException error) {
+                Log.v("Malformed URL", "Malformed URL");
+            } catch (SocketTimeoutException error) {
+                Log.v("Socket Timeout","Socket Timeout");
+            } catch (IOException error) {
+                Log.v("Socket Timeout","Socket Timeout");
+            } finally {
+                if (client != null)
+                    client.disconnect();
+            }
+
+            //Send the built string. After doInBackground executes it goes to onPostExecute
+            return replyFromServer.toString();
+        }
+
+        //Update the TextView UI element with whatever the server send back
+        @Override
+        protected void onPostExecute(String result) {
+
+            replyFromServer.setText(result);
+        }
+    }
+}
